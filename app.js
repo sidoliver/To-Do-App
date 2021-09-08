f27bd06af8f1819cfd8405ac0928200083b5188d
sidoliver <sidchoice@gmail.com>
2021-09-08 09:23:06 +0530

    styled the form and added javascript functionality

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 const plusBtn = document.querySelector('.plus')
+const favBtn = document.querySelector('.fav-btn')
+const cancelBtn = document.querySelector('.cancel')
+const addBtn = document.querySelector('.add')
+const mainColor = document.querySelector('.main-clr')
+const circle = document.querySelector('.circle')
 
 
-let addNewProject = function() {
-    console.log('I am ready')
+let openForm = function() {
+    const projectForm = document.querySelector('.project-form')
+    projectForm.classList.add('show-form')
 }
-plusBtn.addEventListener('click',addNewProject)
+const changeColor = function() {
+    
+    mainColor.classList.toggle('main-clr-animation')
+    circle.classList.toggle('circle-animation')
+}
+const hideForm = function() {
+    const projectForm = document.querySelector('.project-form')
+    projectForm.classList.remove('show-form')
+    mainColor.classList.remove('main-clr-animation')
+    circle.classList.remove('circle-animation')
+
+}
+plusBtn.addEventListener('click',openForm)
+cancelBtn.addEventListener('click',hideForm)
+// addBtn.addEventListener('click',addNewProject)
+favBtn.addEventListener('click',changeColor)
